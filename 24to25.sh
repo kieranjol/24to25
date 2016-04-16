c1650a953ac00a987f5560b6c024da115907c9c0
Kieran O'Leary <kieran.o.leary@gmail.com>
2016-04-16 12:18:55 +0100

    Finally implement @retokromer improvement.
    Disappointed in myself that I waited this long.

--- 24to25.sh
+++ 24to25.sh
@@ -1,8 +1,3 @@
 #!/bin/bash -x 
 
-
- ffmpeg -i "$1" -r 25 -filter:v "setpts=0.96*PTS" -c:v prores -an "$1"_silent.mov
- ffmpeg -i "$1" -filter:a "atempo=25/24" -vn "$1"_audio.wav
- ffmpeg -i "$1"_silent.mov -i "$1"_audio.wav -c copy "$1"_retimedtopal.mov
-
-# ffmpeg -i "$1" -r 25 -filter_complex "[0:v]setpts=24/25*PTS[v]; [0:a]atempo=25/24[a]" -map "[v]" -map "[a]" "$1"_redimedtopal.mov
+ffmpeg -i "$1" -r 25 -filter_complex "[0:v]setpts=24/25*PTS[v]; [0:a]atempo=25/24[a]" -map "[v]" -map "[a]" "$1"_redimedtopal.mov
